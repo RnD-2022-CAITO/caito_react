cde93b595c85069f211b11193d264fba4c21f152
danieltran-512 <xkw1927@autuni.ac.nz>
2022-05-03 09:31:36 +1200

    sign up bug fixed

--- src/components/global/auth/Authentication.js
+++ src/components/global/auth/Authentication.js
@@ -16,7 +16,7 @@ export const AuthProvider = ({children}) => {
 
     //call the auth function from firebase
     function signUp(user){
-        auth.createUserWithEmailAndPassword(user.email,user.password)
+        return auth.createUserWithEmailAndPassword(user.email,user.password)
         .then(async u => {
             // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
             // key is the counterpart to the secret key you set in the Firebase console.
