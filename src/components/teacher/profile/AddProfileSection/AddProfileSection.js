eaa3be953715c47bb4d2bbd4acc1455afb752365
PatrickPei747 <754463323@qq.com>
2022-05-16 15:27:06 +1200

    update

--- src/components/teacher/profile/AddProfileSection/AddProfileSection.js
+++ src/components/teacher/profile/AddProfileSection/AddProfileSection.js
@@ -5,7 +5,7 @@ import { useAuth } from '../../../global/auth/Authentication.js'
 import "./AddProfileSection.css"
 
 
-const AddProfileSection = () => {
+const AddProfileSection = (props) => {
 
     const {currentUser} = useAuth();
     const {userData} = useUserData();
@@ -26,30 +26,96 @@ const AddProfileSection = () => {
         }
     }
 
-    //add textArea for About
-    const addAbout = () => {
-       alert(111);
+    //show textArea for About
+    const showAbout = () => {
+        let about = document.getElementById('aboutText');
+        if  (about.style.display = "none") {
+            about.style.display = "block";
+        }
+        let title = document.getElementById('about-title');
+        if  (title.style.display = "none") {
+            title.style.display = "block";
+        }
+        document.getElementById('about').style.color='grey';
+        close();
     }
 
-    //add textArea for Education
-    const addEducation = () => {
-        alert(111);
+    //show textArea for Education
+    const showEducation = () => {
+        let education = document.getElementById('educationText');
+        if  (education.style.display = "none") {
+            education.style.display = "block";
+        }
+        let title = document.getElementById('education-title');
+        if  (title.style.display = "none") {
+            title.style.display = "block";
+        }
+        document.getElementById('education').style.color='grey';
+        close();
      }
 
-     //add textArea for Skills
-     const addSkills = () => {
-        alert(111);
+     //show textArea for Skills
+     const showSkills = () => {
+        let skills = document.getElementById('skillsText');
+        if  (skills.style.display = "none") {
+            skills.style.display = "block";
+        }
+        let title = document.getElementById('skills-title');
+        if  (title.style.display = "none") {
+            title.style.display = "block";
+        }
+        document.getElementById('skills').style.color='grey';
+        close();
      }
 
-     //add textArea for Working experience
-     const addWorkingEx = () => {
-        alert(111);
+     //show textArea for Working experience
+     const showWorkingEx = () => {
+        let workingEx = document.getElementById('workingExText');
+        if  (workingEx.style.display = "none") {
+            workingEx.style.display = "block";
+        }
+        let title = document.getElementById('workingEx-title');
+        if  (title.style.display = "none") {
+            title.style.display = "block";
+        }
+        document.getElementById('working-ex').style.color='grey';
+        close();
      }
 
-     //add textArea for Hobbies
-     const addHobbies = () => {
-        alert(111);
+     //show textArea for Hobbies
+     const showHobbies = () => {
+        let hobbies = document.getElementById('hobbiesText');
+        if  (hobbies.style.display = "none") {
+            hobbies.style.display = "block";
+        }
+        let title = document.getElementById('hobbies-title');
+        if  (title.style.display = "none") {
+            title.style.display = "block";
+        }
+        document.getElementById('hobbies').style.color='grey';
+        close();
      }
+
+     //initially the sections are read only, click button to edit
+     const enableEdit = (e) => {
+        switch(e) {
+            case 'about':
+                document.getElementById('aboutText').removeAttribute('readOnly');
+                break;
+            case 'education':
+                document.getElementById('educationText').removeAttribute('readOnly');
+                break;
+            case 'skills':
+                document.getElementById('skillsText').removeAttribute('readOnly');
+                break;
+            case 'workingEx':
+                document.getElementById('workingExText').removeAttribute('readOnly');
+                break;
+            case 'hobbies':
+                document.getElementById('hobbiesText').removeAttribute('readOnly');
+                break;
+        }
+    }
 
     return (
          <div>
@@ -58,6 +124,43 @@ const AddProfileSection = () => {
                 <h3>{userData.firstName} {userData.lastName}</h3>
                 <p>Email: {currentUser.email}</p>
                 <button className='add-section' onClick={show}>Add Profile Section</button>
+                <div id='seciton-text'>
+                    <p id='about-title'>
+                        About
+                        <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'about')}></img>
+                        </p>
+                    <textarea id='aboutText' readOnly="readonly">
+                    </textarea><br />
+                    
+                    <p id='education-title'>
+                        Education
+                        <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'education')}></img>
+                    </p>
+                    <textarea id='educationText' readOnly="readonly">
+                    </textarea><br />
+                    
+                    <p id='skills-title'>
+                        Skills
+                        <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'skills')}></img>
+                        </p>
+                    <textarea id='skillsText' readOnly="readonly">  
+                    </textarea><br />
+                    
+                    <p id='workingEx-title'>
+                        Working Experience
+                        <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'workingEx')}></img>
+                        </p>
+                    <textarea id='workingExText' readOnly="readonly">
+                    </textarea><br />
+                    
+                    <p id='hobbies-title'>
+                        Hobbies
+                        <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'hobbies')}></img>
+                        </p>
+                    <textarea id='hobbiesText' readOnly="readonly">
+                    </textarea><br />
+                </div>
+                
             </div> 
             
             {/* dialog UI */}
@@ -68,11 +171,11 @@ const AddProfileSection = () => {
                         <button className='close-btn' onClick={close}>x</button>
                     </div>
                     <div id='section'>
-                        <p id='about' onClick={addAbout}>About</p><hr />
-                        <p id='education' onClick={addEducation}>Education</p><hr />
-                        <p id='skills' onClick={addSkills}>Skills</p><hr />
-                        <p id='working-ex' onClick={addWorkingEx}>Working experience</p><hr />
-                        <p id='hobbies' onClick={addHobbies}>Hobbies</p>
+                        <p id='about' onClick={showAbout}>About</p><hr />
+                        <p id='education' onClick={showEducation}>Education</p><hr />
+                        <p id='skills' onClick={showSkills}>Skills</p><hr />
+                        <p id='working-ex' onClick={showWorkingEx}>Working experience</p><hr />
+                        <p id='hobbies' onClick={showHobbies}>Hobbies</p>
                         
                     </div>
                 </div>
