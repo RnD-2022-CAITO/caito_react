ec6486610ae1d6a023c378440283096b14b85d8f
PatrickPei747 <754463323@qq.com>
2022-05-17 12:30:59 +1200

    update

--- src/components/teacher/profile/AddProfileSection/AddProfileSection.js
+++ src/components/teacher/profile/AddProfileSection/AddProfileSection.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { useUserData } from '../../../global/auth/UserData.js'
 import { useAuth } from '../../../global/auth/Authentication.js'
+import app,{func} from '../../../../utils/firebase'
+import { useNavigate } from 'react-router-dom'
+import { useRef } from 'react'
 
 import "./AddProfileSection.css"
+
+const site_key = '6Lf6lbQfAAAAAIUBeOwON6WgRNQvcVVGfYqkkeMV';
 
 
 const AddProfileSection = (props) => {
 
+    const navigate = useNavigate();
     const {currentUser} = useAuth();
     const {userData} = useUserData();
+    const aboutRef = useRef();
+
+
 
     //show the add section dialog when user click button
     const show = () => {
@@ -36,6 +45,10 @@ const AddProfileSection = (props) => {
         if  (title.style.display = "none") {
             title.style.display = "block";
         }
+        let btn = document.getElementById('save-about')
+        if  (btn.style.display = "none") {
+            btn.style.display = "block";
+        }
         document.getElementById('about').style.color='grey';
         close();
     }
@@ -49,6 +62,10 @@ const AddProfileSection = (props) => {
         let title = document.getElementById('education-title');
         if  (title.style.display = "none") {
             title.style.display = "block";
+        }
+        let btn = document.getElementById('save-education')
+        if  (btn.style.display = "none") {
+            btn.style.display = "block";
         }
         document.getElementById('education').style.color='grey';
         close();
@@ -64,6 +81,10 @@ const AddProfileSection = (props) => {
         if  (title.style.display = "none") {
             title.style.display = "block";
         }
+        let btn = document.getElementById('save-skills')
+        if  (btn.style.display = "none") {
+            btn.style.display = "block";
+        }
         document.getElementById('skills').style.color='grey';
         close();
      }
@@ -78,6 +99,10 @@ const AddProfileSection = (props) => {
         if  (title.style.display = "none") {
             title.style.display = "block";
         }
+        let btn = document.getElementById('save-workingEx')
+        if  (btn.style.display = "none") {
+            btn.style.display = "block";
+        }
         document.getElementById('working-ex').style.color='grey';
         close();
      }
@@ -91,6 +116,10 @@ const AddProfileSection = (props) => {
         let title = document.getElementById('hobbies-title');
         if  (title.style.display = "none") {
             title.style.display = "block";
+        }
+        let btn = document.getElementById('save-hobbies')
+        if  (btn.style.display = "none") {
+            btn.style.display = "block";
         }
         document.getElementById('hobbies').style.color='grey';
         close();
@@ -117,6 +146,20 @@ const AddProfileSection = (props) => {
         }
     }
 
+    const saveAbout= async() => {
+        app.appCheck().activate(site_key, true);
+            const addAboutTxt = func.httpsCallable('teacher-addBioSection');
+            try {
+                const response = await addAboutTxt({
+                    sectionName: "About",
+                    sectionData: aboutRef.current.value,
+                });
+                navigate("/profile");
+            } catch (e) {
+                console.error(e);
+            }
+    }
+
     return (
          <div>
             <div className='more-section'>
@@ -125,40 +168,46 @@ const AddProfileSection = (props) => {
                 <p>Email: {currentUser.email}</p>
                 <button className='add-section' onClick={show}>Add Profile Section</button>
                 <div id='seciton-text'>
+                    About: {userData.About}
                     <p id='about-title'>
                         About
                         <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'about')}></img>
                         </p>
-                    <textarea id='aboutText' readOnly="readonly">
-                    </textarea><br />
+                    <textarea id='aboutText' readOnly="readonly" ref={aboutRef}>
+                    </textarea>
+                    <button id='save-about' onClick={saveAbout}>Save</button><br />
                     
                     <p id='education-title'>
                         Education
                         <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'education')}></img>
                     </p>
                     <textarea id='educationText' readOnly="readonly">
-                    </textarea><br />
+                    </textarea>
+                    <button id='save-education'>Save</button><br /><br />
                     
                     <p id='skills-title'>
                         Skills
                         <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'skills')}></img>
                         </p>
                     <textarea id='skillsText' readOnly="readonly">  
-                    </textarea><br />
+                    </textarea>
+                    <button id='save-skills'>Save</button><br /><br />
                     
                     <p id='workingEx-title'>
                         Working Experience
                         <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'workingEx')}></img>
                         </p>
                     <textarea id='workingExText' readOnly="readonly">
-                    </textarea><br />
+                    </textarea>
+                    <button id='save-workingEx'>Save</button><br /><br />
                     
                     <p id='hobbies-title'>
                         Hobbies
                         <img src={require('./images/edit.png')} onClick={enableEdit.bind(this, 'hobbies')}></img>
                         </p>
                     <textarea id='hobbiesText' readOnly="readonly">
-                    </textarea><br />
+                    </textarea>
+                    <button id='save-hobbies'>Save</button><br /><br />
                 </div>
                 
             </div> 
