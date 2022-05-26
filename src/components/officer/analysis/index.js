ebd676021d5d06c959c8281a0a6ef59621bed4dc
danieltran-512 <xkw1927@autuni.ac.nz>
2022-05-27 08:26:32 +1200

    search bar in summary page

--- src/components/officer/analysis/index.js
+++ src/components/officer/analysis/index.js
@@ -1,5 +1,5 @@
 //main page for the components
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import app, {func} from '../../../utils/firebase';
 import { db } from '../../../utils/firebase';
 
@@ -11,6 +11,11 @@ const site_key = '6Lf6lbQfAAAAAIUBeOwON6WgRNQvcVVGfYqkkeMV';
 const OfficerSummary = () => {
   const [questionID, setQuestionID] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const searchRef = useRef();
+  const [search, setSearch] = useState(false);
+  //Pattern for search engine
+  const [pattern, setPattern] = useState('');
 
   // const {currentUser} = useAuth();
   // const [questionID, setQuestionID] = useState([]);
@@ -36,7 +41,6 @@ const OfficerSummary = () => {
   //Get surveys
   useEffect(() => {
       const retrieveAnswers =  async () => {
-        console.log('run')
         if(questionID.length>1){
           const newArr = await Promise.all(questionID.map(async (question) => {
             var total = 0;
@@ -75,6 +79,28 @@ const OfficerSummary = () => {
     alert('this feature has not been developed.')
   }
 
+  const searchSurvey = () => {
+    console.log(searchRef.current.value);
+
+    setSearch(true);
+
+
+    //Set new regular expression
+    const regex = `^.*${searchRef.current.value}.*$`;
+
+    setPattern(new RegExp(regex));
+
+    const str = 'abd lalala';
+
+    console.log(str.includes(searchRef.current.value));
+
+    if(!searchRef.current.value.trim()){
+
+      setSearch(false);
+    }
+
+  }
+
   return (
     loading ? 
     <p>Loading..</p>
@@ -82,8 +108,14 @@ const OfficerSummary = () => {
     <div>
         <h1 style={{textAlign:'center'}}>Survey Summary</h1>
 
+        <div className='search-engine'>
+          <input type="text" ref={searchRef} placeholder="Type survey name to search"/>
+          <button id="search-btn" onClick={searchSurvey}>Search</button>
+        </div>
+        {!search ? 
+        //Display all surveys
+        <>
         <h5 style={{textAlign:'center'}}>You have {questionID.length} survey(s) in total</h5>
-
         {questionID.map(question => (
           <div key={question.id}>
                 <div className='summary-view'>
@@ -96,6 +128,26 @@ const OfficerSummary = () => {
               </div>
           </div>
         ) )}
+        </>
+        :
+        //display surveys that matches with the pattern on the search engine 
+        <>
+        <h5 style={{textAlign:'center'}}>Search matched with "{searchRef.current.value}"</h5>
+
+        {questionID.map(question => (
+          question.title.toLowerCase().includes(searchRef.current.value.toLowerCase()) &&
+          <div key={question.id}>
+                <div className='summary-view'>
+                <h3>{question.title}</h3>
+                <h4>Question ID: {question.id}</h4>
+                <p>Total surveys sent out: {question.total}</p>
+                <p>Total teachers submitted: {question.complete}</p>
+                <p>Completion rate: {question.total != 0 ? question.complete/question.total * 100 + " %" : "You haven't distribute this survey yet"}</p>
+                <button className='summary-btn' onClick={() => clickButton(question)}>View details</button>
+              </div>
+          </div>
+        ) )}
+        </>}
     </div>
   )
 }
