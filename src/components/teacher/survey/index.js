8ecbbb5e1828f4653860edb41440eafbb2db7c72
prunellaparisa <prunella.poiney@gmail.com>
2022-07-21 19:18:43 +1200

    unable to populate initial database answers
    able to retrieve db answers but not populate it

--- src/components/teacher/survey/index.js
+++ src/components/teacher/survey/index.js
@@ -44,11 +44,9 @@ const Survey = () => {
           const response = await getSurvey({
             questionID: location.state.questionID,
           });
-          
           if(response.data == null){
             setFound(false);
           } else {
-
             //Assign survey details to the variables
             setTitle(response.data.title);
             setQuestions(response.data.questions);
@@ -66,34 +64,54 @@ const Survey = () => {
                   if(doc.data().teacherID === currentUser.uid){
                     setAnswerID(doc.id);
                   }
-
+                  console.log("doc.id: " + doc.id);
+                  populateExistingAnswers(doc.id);
                   return doc.id;
               })
-          })
+          });
 
+          
       } catch (e) {
           console.error(e);
       }
     }
 
     retrieveSurvey();
+
     // eslint-disable-next-line
   }, []);
 
   //Update checkbox values
   useEffect(() => {
     if(index!==''){
-
       let newArr = [...answers];
-  
       newArr[index] = Object.assign({}, checkboxVal);
-  
       setAnswers(newArr);
     }
-
     // eslint-disable-next-line
   }, [checkboxVal])
 
+  //populate existing answers upon page initialization
+  const populateExistingAnswers = async (id) => {
+    app.appCheck().activate(site_key, true);
+    const getAnswers = func.httpsCallable('teacher-getAssignedSurvey_Answers');
+    try {
+        getAnswers({
+        answerID: id
+      }).then((i) => {
+        let newArr = [...i.data.answers];
+        if (newArr.length > 1){
+          setAnswers(newArr);
+          console.log("newArr: " + newArr); // <- this updates TODO
+          console.log("answers: "+ answers); // <- this doesn't. updates too slow. how to populate existing answers?
+        }
+      }).catch(e => {
+        console.log(e);
+      });
+    } catch (e) {
+        console.error(e);
+    }
+  }
 
   //save the current answer to the answers array
   const saveAnswer = (e, index, type) => {
@@ -121,29 +139,29 @@ const Survey = () => {
       //Save to the answers array
       setAnswers(newArr);
     }
-
   }
 
   //Submit survey to the server 
-  const sendSurvey = async (e) => {
+  const sendSurvey = async (e, boolean) => {
     e.preventDefault();
 
-    await updateSurvey();
+    updateSurvey(boolean);
 
-    alert('You have submitted the survey!');
+    alert('You have submitted/saved the survey!');
 
     navigate('/');
     
   }
 
-  async function updateSurvey(){
+  const updateSurvey = (boolean) => {
     app.appCheck().activate(site_key, true);
     const getSurveys = func.httpsCallable('teacher-updateAssignedSurvey_Answers');
     try {
         setLoading(true);
-        await getSurveys({
+        getSurveys({
           answerID: answerID,
-          answers: answers
+          answers: answers,
+          isSubmitted: boolean
         });
         setLoading(false);
     } catch (e) {
@@ -166,7 +184,7 @@ const Survey = () => {
 
   return (
     isFound ?
-    <form className='survey' onSubmit={e => sendSurvey(e)}>
+    <form className='survey' onSubmit={e => sendSurvey(e, true)}>
        {!formLoading ? 
               <>
               <h1>Survey: {surveyTitle}</h1>
@@ -179,18 +197,19 @@ const Survey = () => {
                    <div >
                     { q.options.map((o) => 
                      <div key={o}>
-                     <input type={q.type} value={o} name={q.type === 'checkbox' ? o : q.question}  onChange={(e) => saveAnswer(e, index, q.type)}></input>
+                     <input type={q.type} value={answers.at(index)} name={q.type === 'checkbox' ? o : q.question}  onChange={(e) => saveAnswer(e, index, q.type)}></input>
                      <label htmlFor={o}>{o}</label>
                      </div>
                      )}
                    </div>
                  : 
                  <div>
-                 <input required type={q.type} placeholder={displayPlaceHolder(q.type)} onChange={(e) => saveAnswer(e, index)}></input>
+                 <input required type={q.type} value={answers.at(index)} placeholder={displayPlaceHolder(q.type)} onChange={(e) => saveAnswer(e, index)}></input>
                  </div>}
                </div>)
                }
                        
+                <button disabled={loading} type='button' onClick={e => sendSurvey(e, false)}>{loading? "Saving..." : "Save And Continue Later"}</button>
                 <button disabled={loading} type='submit'>{loading? "Submitting..." : "Complete"}</button>
               </>
        : <p>Loading...</p>}
