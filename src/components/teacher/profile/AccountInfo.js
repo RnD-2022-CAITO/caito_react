9a10398b81415764a1855b1342a9f2369193636e
Gary <122460790xyz@gmail.com>
2022-08-03 23:10:29 +1200

    test

--- src/components/teacher/profile/AccountInfo.js
+++ src/components/teacher/profile/AccountInfo.js
@@ -7,7 +7,6 @@ const AccountInfo = (props) => {
     return (
         <div className='profile-section'>
             <h3>{data.firstName} {data.lastName}</h3>
-            <h4>Graduate School:  {data.school}</h4>
             <h5 style={{textTransform:'capitalize'}}>{data.role}</h5>
             <p>Email: {user.email}</p>
             <Link to='/edit-password'>
