a95faaa734c2bc0028f2ed75bb9d5c02cee535c0
danieltran-512 <xkw1927@autuni.ac.nz>
2022-08-04 14:00:50 +1200

    UI updates & delete question

--- src/components/officer/surveyDistribution/surveyDistribution.js
+++ src/components/officer/surveyDistribution/surveyDistribution.js
@@ -136,6 +136,7 @@ const OfficerSurveyDistribution = () => {
         <h3>Select your profiling task</h3>
         <div className=' template input-field'>
           <select 
+            value={selectedSurveys}
             onChange={e => 
               {
                 setSelectedSurveys(e.target.value);
@@ -152,6 +153,10 @@ const OfficerSurveyDistribution = () => {
             Select a profiling task
           </label>
         </div>
+
+        <p>
+          <em>It may take a couple of seconds to load the tasks, please be patient.</em>
+        </p>
 
         {/* {surveyDisplay ?
         
