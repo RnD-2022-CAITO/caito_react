fd915000e9067331fefbbc842e84319d00dc5217
prunellaparisa <prunella.poiney@gmail.com>
2022-09-05 14:14:42 +1200

    first draft of seeSubmittedAnswers survey pagination

--- src/components/teacher/landing/Pagination.js
+++ src/components/teacher/landing/Pagination.js
@@ -1,23 +1,31 @@
-import React from 'react'
-import './teacherLanding.css'
+import React from "react";
+import "./teacherLanding.css";
 
-export const Pagination = ({ taskPerPage, totalTasks, setCurrentPage, currentPage}) => {
+export const Pagination = ({
+  taskPerPage,
+  totalTasks,
+  setCurrentPage,
+  currentPage,
+}) => {
+  const pageNumbers = [];
 
-    const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(totalTasks / taskPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
-    for(let i = 1; i <= Math.ceil(totalTasks / taskPerPage); i++){
-        pageNumbers.push(i);
-    }
-
-    return (
-        <nav>
-            <ul className="pagination">
-                {pageNumbers.map(number => (
-                    <button 
-                    className={number === currentPage ? "active" : ""}
-                    key={number} onClick={() => setCurrentPage(number)}>{number}</button>
-                ))}
-            </ul> 
-        </nav>
-    )
-}
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <button
+            className={number === currentPage ? "active" : ""}
+            key={number}
+            onClick={() => setCurrentPage(number)}
+          >
+            {number}
+          </button>
+        ))}
+      </ul>
+    </nav>
+  );
+};
